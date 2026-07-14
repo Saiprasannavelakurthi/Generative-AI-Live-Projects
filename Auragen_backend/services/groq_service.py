1fe1614f6f044a6c310fe21e1a9a5627d18c650d
KARKUVEL P <pgkarkuvel003@gmail.com>
2026-07-14 17:11:43 +0530

    Add error handling

--- Auragen_backend/services/groq_service.py
+++ Auragen_backend/services/groq_service.py
@@ -1,7 +1,5 @@
 from langchain_groq import ChatGroq
-
 from config import GROQ_API_KEY, MODEL_NAME
-
 
 class GroqService:
 
@@ -11,14 +9,18 @@ class GroqService:
             model=MODEL_NAME,
             api_key=GROQ_API_KEY,
             temperature=0.2,
-            max_tokens=2048
+            max_tokens=2026
         )
 
     def generate(self, prompt):
+        try:
+            response = self.llm.invoke(prompt)
+            return response.content
 
-        response = self.llm.invoke(prompt)
-
-        return response.content
+        except Exception as e:
+            return {
+                "error": str(e)
+            }
 
 
 # Singleton instance
